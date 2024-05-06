019358ed1dd625fe1910649f6b66b5dfdc9266a5
Mattia Effendi <contact@mattiaeffendi.me>
2024-05-06 15:03:32 +0200

    test: test pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Test deploy</h1>
+      <h1>Se sono stato bravo questa cosa dovrebbe andare su in automatico</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
